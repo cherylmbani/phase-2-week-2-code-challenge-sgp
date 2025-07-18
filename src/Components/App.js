7cb075784a3e119e0b96a930fc0a99891cd37203
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-07-18 14:08:45 +0300

    fetching gaols from db.json in App.js and apssing it as props to GoalList component

---
+++ src/Components/App.js
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from 'react'
+import GoalList from "./GoalList.js"
+
+const App = () => {
+  const [goals, setGoals]=useState(null);
+  useEffect(()=>{
+    fetch("http://localhost:3001/goals")
+    .then(r=>r.json())
+    .then(data=>{
+      setGoals(data);
+      console.log(data);
+    })
+  }, [])
+  return (
+    <div>
+      <header>
+        <h1>Smart Goal Planner</h1>
+      </header>
+      <main>
+        <GoalList goals={goals} />
+      </main>
+
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/GoalList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const GoalList = ({goals}) => {
+    const goalsArray=goals.map((gaol)=>{
+        
+    })
+  return (
+    <div>GoalList</div>
+  )
+}
