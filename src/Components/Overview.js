984a2499edb7b7a7bb4cec409b6ccdca72a615e8
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-07-19 13:40:44 +0300

    adding overdue and warning

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -20,6 +20,23 @@ function Overview({goals}){
             daysLeft:daysLeft
         };
     })
+
+    const warnings=goals.filter(goal=>{
+        const deadlineDate = new Date(goal.deadline);
+        const today = new Date();
+        const daysLeft = Math.ceil((deadlineDate - today) / (1000 * 60 * 60 * 24));
+        const isComplete=Number(goal.savedAmount)>=Number(goal.targetAmount);
+        return daysLeft<=30 &&daysLeft>0 && !isComplete;
+
+    });
+
+    const overdue=goals.filter(goal=>{
+        const deadlineDate = new Date(goal.deadline);
+        const today = new Date();
+        const daysLeft = Math.ceil((deadlineDate - today) / (1000 * 60 * 60 * 24));
+        const isComplete=Number(goal.savedAmount)>=Number(goal.targetAmount);
+        return daysLeft<0 &&!isComplete;
+    });
   return (
     <div>
         <h2>Overview</h2>
@@ -34,6 +51,30 @@ function Overview({goals}){
                     <b>{goal.name}:</b> {goal.daysLeft} {goal.daysLeft===1?"day":"days"} left
                 </li>
             ))}
+        </ul>
+        <h3 style={{color:"orange"}}>Goals Approaching Deadline</h3>
+        <ul>
+            {warnings.length===0?(
+                <li>No Warnings</li>
+            ):(
+                warnings.map(goal=>(
+                    <li key={goal.id}>
+                        <b>{goal.name}</b> -Less than 30 days left!
+                    </li>
+                ))
+            )}
+        </ul>
+        <h3 style={{color:"red"}}> Overdue Goals</h3>
+        <ul>
+            {overdue.length===0?(
+                <li>No overdue goals</li>
+            ) : (
+                overdue.map(goal=>(
+                    <li key={goal.id}>
+                        <b>{goal.name}</b> - Dealine passed without completion!
+                    </li>
+                ))
+            )}
         </ul>
 
     </div>
