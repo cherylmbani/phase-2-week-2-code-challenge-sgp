663ebb52661635fcef15f65dad56ad04929cc6bb
Cheryl Mbani <mbanicheryl45@gmail.com>
2025-07-19 13:14:32 +0300

    looping through each goal to calculate number of days left before deadline, using map iteration method

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -8,12 +8,33 @@ function Overview({goals}){
     const completedGoal=goals.filter((goal)=>{
         return Number(goal.savedAmount)>=Number(goal.targetAmount);
     })
+
+    const remainingTime=goals.map((goal)=>{
+        const deadlineDate=new Date(goal.deadline);
+        const today= new Date();
+        const timeDifference=deadlineDate-today;
+        const daysLeft=Math.ceil(timeDifference /(1000 * 60 * 60 *24));
+
+        return{
+            name:goal.name,
+            daysLeft:daysLeft
+        };
+    })
   return (
     <div>
         <h2>Overview</h2>
         <p><b>Total number of goals:</b> {goals.length}</p>
         <p><b>Total Amount Saved:</b> {totalSavings}</p>
         <p><b>Total Number of Goals Completed:</b> {completedGoal.length}</p>
+        
+        <h3>Days Left Per Goal</h3>
+        <ul>
+            {remainingTime.map((goal,index)=>(
+                <li key={index}>
+                    <b>{goal.name}:</b> {goal.daysLeft} {goal.daysLeft===1?"day":"days"} left
+                </li>
+            ))}
+        </ul>
 
     </div>
   )
